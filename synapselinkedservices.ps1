79d68e9313338c65b20ec0688ecb88da508e81bd
sureshyadav1973 <68124819+sureshyadav1973@users.noreply.github.com>
2022-02-18 12:02:27 -0600

    Update synapselinkedservices.ps1

--- synapselinkedservices.ps1
+++ synapselinkedservices.ps1
@@ -1,8 +1,7 @@
 param(
 
   [string] $SynapseWorkspaceName,
-   [string] $KeyVaultName,
-  [string] $UAMIIdentityID
+   [string] $KeyVaultName
   
 )
 
@@ -88,25 +87,7 @@ function Save-SynapseLinkedService{
 $retries = 10
 $secondsDelay = 60
 
-#------------------------------------------------------------------------------------------------------------
-# CONTROL PLANE OPERATION: ASSIGN SYNAPSE WORKSPACE ADMINISTRATOR TO USER-ASSIGNED MANAGED IDENTITY
-# UAMI needs Synapse Admin rights before it can make calls to the Data Plane APIs to create Synapse objects
-#------------------------------------------------------------------------------------------------------------
 
-$token = (Get-AzAccessToken -Resource "https://dev.azuresynapse.net").Token
-$headers = @{ Authorization = "Bearer $token" }
-
-$uri = "https://$SynapseWorkspaceName.dev.azuresynapse.net/rbac/roleAssignments?api-version=2020-02-01-preview"
-
-#Assign Synapse Workspace Administrator Role to UAMI
-$body = "{
-  roleId: ""6e4bf58a-b8e1-4cc3-bbf9-d73143322b78"",
-  principalId: ""$UAMIIdentityID""
-}"
-
-Write-Host "Assign Synapse Administrator Role to UAMI..."
-
-Invoke-RestMethod -Method Post -ContentType "application/json" -Uri $uri -Headers $headers -Body $body
 
 
 #------------------------------------------------------------------------------------------------------------
